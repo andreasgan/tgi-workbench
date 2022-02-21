ed95750e28b566c8c79dcee36ce68d782ef5c2f2
Andreas Gangsø <andreasgangso@gmail.com>
2022-02-21 20:27:00 +0000

    stuff

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+localStorage.debug = 'socket.io-client:socket'
 class Game extends Phaser.Scene {
     constructor () {
         super('Boot');
@@ -13,6 +14,7 @@ class Game extends Phaser.Scene {
 		this.load.image('terrain', 'assets/map/terrain.png');
 		this.load.tilemapTiledJSON('mapa', 'assets/map/2.json');
 		this.load.image('bullet', 'assets/spikeball.png');
+		this.socket = io("https://tgi-workbench.tgi-koding.repl.co/");
     }
 
     create(data) {
@@ -115,13 +117,17 @@ class Game extends Phaser.Scene {
 			}
 		}
 		if (Phaser.Input.Keyboard.JustDown(keys.space)) {
-			let bullet = this.physics.add.image(player.body.position.x+10, player.body.position.y+20, 'bullet')
+			let bullet = this.physics.add.image(player.body.position.x+10, player.body.position.y+15, 'bullet')
 			this.bullets.add(bullet)
 			bullet.setVelocityX(player.flipX ? -400 : 400)
 		}
 		if (player.body.velocity.x === 0 && player.body.velocity.y === 0) {
 			player.play('mask-dude-idle', true)
 		}
+		this.socket.emit('info', {
+			x: player.body.position.x
+			y: player.body.position.y
+		})
 		
     }
 }
